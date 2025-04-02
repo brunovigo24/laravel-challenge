9c3afce1a59d50ff276b0cdb61282efa900adfaa
brunovigo24 <brunohvigo@gmail.com>
2025-04-02 20:32:05 -0300

    refactor: Simplify client routes by using resource controller

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 use App\Http\Controllers\ClientController;
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
-    Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
-    Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
-
-    Route::get('/clients/{client}', [ClientController::class, 'show'])->name('clients.show');
-    Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
-    Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
-    Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
+    Route::resource('clients', ClientController::class);
 });
